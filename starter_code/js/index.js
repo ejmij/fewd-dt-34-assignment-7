36d9b1e9f2ef7ec2da517b785a7fd5de902c6f7a
ejmij <36144255+ejmij@users.noreply.github.com>
2018-04-16 21:19:18 -0700

    submit

---
+++ starter_code/js/index.js
@@ -0,0 +1,15 @@
+$(".cross").hide();
+$(".mobile-menu").hide();
+$(".hamburger").click(function() {
+  $(".mobile-menu").slideToggle("slow",function() {
+    $(".hamburger").hide();
+    $(".cross").show();
+  });
+});
+
+$(".cross").click(function() {
+  $(".mobile-menu").slideToggle("slow", function() {
+    $(".cross").hide();
+    $(".hamburger").show();
+  });
+});
